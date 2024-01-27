628963a2945da844ee94dda3fcfb750353ba6b92
Shubham Mapari <36085347+coderastic@users.noreply.github.com>
2024-01-27 14:46:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,56 @@
 import os
+import cv2
 import tempfile
+import numpy as np
 from flask import Flask, request, send_file, render_template
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Render the main page with the form to upload a video and enter watermark text
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_video():
     video_file = request.files['video']
     watermark_text = request.form['watermark']
+    output_filename = 'output_video.mp4'
 
-    # Save the uploaded file to a temporary file
     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_video:
         video_file.save(temp_video)
         temp_video_path = temp_video.name
 
     try:
-        processed_video = add_watermark(temp_video_path, watermark_text)
-
-        output_filename = 'output_video.mp4'
-        processed_video.write_videofile(output_filename, codec='libx264', audio_codec='aac')
-
+        add_watermark(temp_video_path, watermark_text, output_filename)
         return send_file(output_filename, as_attachment=True)
     finally:
-        # Clean up: remove the temporary files
         os.remove(temp_video_path)
         if os.path.exists(output_filename):
             os.remove(output_filename)
 
-def add_watermark(video_file_path, watermark_text):
-    # Process the video and add a floating watermark
-    clip = VideoFileClip(video_file_path)
+def add_watermark(input_video_path, watermark_text, output_video_path):
+    cap = cv2.VideoCapture(input_video_path)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
 
-    txt_clip = TextClip(watermark_text, fontsize=70, color='white')
-    txt_mov = txt_clip.set_position(lambda t: ('center', 50*t)).set_duration(clip.duration)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    font_color = (255, 255, 255)
+    font_thickness = 2
 
-    video = CompositeVideoClip([clip, txt_mov])
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    return video
+        cv2.putText(frame, watermark_text, (50, 50), font, font_scale, font_color, font_thickness, cv2.LINE_AA)
+        out.write(frame)
+
+    cap.release()
+    out.release()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable if available, else default to 5000
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-

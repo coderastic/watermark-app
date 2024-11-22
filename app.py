bcdbeecc569c434f25b33416b5f1aa82989f4832
Shubham Mapari <36085347+coderastic@users.noreply.github.com>
2024-11-22 18:53:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,5 @@ def cleanup_files(file_paths):
         if os.path.exists(file_path):
             os.remove(file_path)
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=7860) 
